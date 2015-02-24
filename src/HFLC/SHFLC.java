7a47bfa052c8bbbf6234c4589a1dfbac8f189693
vai2liz <vai2liz@gmail.com>
2015-02-24 01:12:50 +0000

    Created obstacle avoidance(rulebase not finished). Finished right wall following

--- src/HFLC/SHFLC.java
+++ src/HFLC/SHFLC.java
@@ -8,8 +8,6 @@ import intervalType2.system.IT2_Rule;
 import intervalType2.system.IT2_Rulebase;
 import tools.JMathPlotter;
 import type1.sets.T1MF_Trapezoidal;
-import type1.system.T1_Antecedent;
-import type1.system.T1_Rule;
 import generic.Input;
 import generic.Output;
 import generic.Tuple;
@@ -25,22 +23,45 @@ import generic.Tuple;
 
 public class SHFLC {
 
-	//Sonar sensor input
+	//Sonar sensor input for wall following
 	Input frontInput;
 	Input backInput;
+	
+	//Sonar sensor input for obstacle avoidance
+	Input obstacleMiddle;
+	Input obstacleLeft;
+	Input obstacleRight;
 	
 	//Output
 	Output direction;
 	
+	//Obstacle avoidance membership function - close
+    T1MF_Trapezoidal obstacleCloseLowerMF;
+    T1MF_Trapezoidal obstacleCloseUpperMF;
+    IntervalT2MF_Trapezoidal obstacleCloseMF;
+    
+    //Obstacle avoidance membership function - far
+    T1MF_Trapezoidal obstacleFarUpperMF;
+    T1MF_Trapezoidal obstacleFarLowerMF;
+    IntervalT2MF_Trapezoidal obstacleFarMF;
+	
     //Wall follow membership function - close
-    T1MF_Trapezoidal closeLowerMF;
-    T1MF_Trapezoidal closeUpperMF;
-    IntervalT2MF_Trapezoidal closeMF;
+    T1MF_Trapezoidal wallCloseLowerMF;
+    T1MF_Trapezoidal wallCloseUpperMF;
+    IntervalT2MF_Trapezoidal wallCloseMF;
     
     //Wall follow membership function - far
-    T1MF_Trapezoidal farUpperMF;
-    T1MF_Trapezoidal farLowerMF;
-    IntervalT2MF_Trapezoidal farMF;
+    T1MF_Trapezoidal wallFarUpperMF;
+    T1MF_Trapezoidal wallFarLowerMF;
+    IntervalT2MF_Trapezoidal wallFarMF;
+    
+    //Obstacle avoidance antecedents
+    IT2_Antecedent closeLeft;
+    IT2_Antecedent farLeft;
+    IT2_Antecedent closeMiddle;
+    IT2_Antecedent farMiddle;
+    IT2_Antecedent closeRight;
+    IT2_Antecedent farRight;
     
     //Wall follow antecedents
     IT2_Antecedent closeFront;
@@ -71,6 +92,7 @@ public class SHFLC {
     //Rulebases
     IT2_Rulebase leftWallRulebase;
     IT2_Rulebase rightWallRulebase;
+    IT2_Rulebase obstacleRulebase;
     
 	/**
 	 * SHFLC Constructor. Creates the Singleton system 
@@ -79,27 +101,53 @@ public class SHFLC {
 	public SHFLC() {
 		
 		//Sonar sensor input for wall following. Definition
-        frontInput = new Input("Sonar", new Tuple(0,100));
-        backInput = new Input("Sonar", new Tuple(0,100));
+        frontInput = new Input("Front sonar", new Tuple(0,100));
+        backInput = new Input("Back sonar", new Tuple(0,100));
+        
+        //Sonar input for obstacle avoidance. Definition
+        obstacleMiddle = new Input("Middle sonar", new Tuple(0, 200));
+        obstacleLeft = new Input("Left sonar", new Tuple(0,200));
+        obstacleRight = new Input("Right sonar", new Tuple(0,200));
         
         //Output definition
         direction = new Output("Direction", new Tuple(-90,90));
         
+        
+        //Obstacle avoidance membership function - close. Definition
+        obstacleCloseLowerMF= new T1MF_Trapezoidal("Lower MF Close",new double[]{0.0, 0.0, 0.0, 80.0});
+        obstacleCloseUpperMF= new T1MF_Trapezoidal("Upper MF Close",new double[]{0.0, 0.0, 80.0, 160.0});
+        obstacleCloseMF = new IntervalT2MF_Trapezoidal("IT2MF Close",obstacleCloseUpperMF,obstacleCloseLowerMF);
+        
+        //Obstacle avoidance membership function - far. Definition
+        obstacleFarUpperMF= new T1MF_Trapezoidal("Upper MF Far",new double[]{40.0, 120.0, 200.0, 200.0});
+        obstacleFarLowerMF= new T1MF_Trapezoidal("Lower MF Far",new double[]{120.0, 200.0, 200.0, 200.0});
+        obstacleFarMF = new IntervalT2MF_Trapezoidal("IT2MF Far",obstacleFarUpperMF,obstacleFarLowerMF);
+        
         //Wall follow membership function - close. Definition
-        closeLowerMF= new T1MF_Trapezoidal("Lower MF Close",new double[]{0.0, 0.0, 0.0, 40.0});
-        closeUpperMF= new T1MF_Trapezoidal("Upper MF Close",new double[]{0.0, 0.0, 40.0, 80.0});
-        closeMF = new IntervalT2MF_Trapezoidal("IT2MF Close",closeUpperMF,closeLowerMF);
+        wallCloseLowerMF= new T1MF_Trapezoidal("Lower MF Close",new double[]{0.0, 0.0, 0.0, 40.0});
+        wallCloseUpperMF= new T1MF_Trapezoidal("Upper MF Close",new double[]{0.0, 0.0, 40.0, 80.0});
+        wallCloseMF = new IntervalT2MF_Trapezoidal("IT2MF Close",wallCloseUpperMF,wallCloseLowerMF);
         
         //Wall follow membership function - far. Definition
-        farUpperMF= new T1MF_Trapezoidal("Upper MF Far",new double[]{20.0, 60.0, 100.0, 100.0});
-        farLowerMF= new T1MF_Trapezoidal("Lower MF Far",new double[]{60.0, 100.0, 100.0, 100.0});
-        farMF = new IntervalT2MF_Trapezoidal("IT2MF Far",farUpperMF,farLowerMF);
+        wallFarUpperMF= new T1MF_Trapezoidal("Upper MF Far",new double[]{20.0, 60.0, 100.0, 100.0});
+        wallFarLowerMF= new T1MF_Trapezoidal("Lower MF Far",new double[]{60.0, 100.0, 100.0, 100.0});
+        wallFarMF = new IntervalT2MF_Trapezoidal("IT2MF Far",wallFarUpperMF,wallFarLowerMF);
+        
+        
+        //Obstacle avoidance antecedents. Definition
+        closeLeft = new IT2_Antecedent("Close Left", obstacleCloseMF, obstacleLeft);
+        farLeft  = new IT2_Antecedent("Far Left", obstacleFarMF, obstacleLeft);
+        closeMiddle = new IT2_Antecedent("Close Middle", obstacleCloseMF, obstacleMiddle);
+        farMiddle = new IT2_Antecedent("Far Middle", obstacleFarMF, obstacleMiddle);
+        closeRight = new IT2_Antecedent("Close Right", obstacleCloseMF, obstacleRight);
+        farRight = new IT2_Antecedent("Far Right", obstacleFarMF, obstacleRight);
+        
         
         //Wall follow antecedents. Definition
-        closeFront = new IT2_Antecedent("Close Front", closeMF, frontInput);
-        farFront = new IT2_Antecedent("Far Front", farMF, frontInput);
-        closeBack = new IT2_Antecedent("Close Back", closeMF, backInput);
-        farBack = new IT2_Antecedent("Far Back", farMF, backInput);
+        closeFront = new IT2_Antecedent("Close Front", wallCloseMF, frontInput);
+        farFront = new IT2_Antecedent("Far Front", wallFarMF, frontInput);
+        closeBack = new IT2_Antecedent("Close Back", wallCloseMF, backInput);
+        farBack = new IT2_Antecedent("Far Back", wallFarMF, backInput);
         
         //Output membership function - turn left. Definition
         leftLowerMF= new T1MF_Trapezoidal("Lower MF Left",new double[]{-72.0, -36.0, -36.0, 0.0});
@@ -122,13 +170,14 @@ public class SHFLC {
         right = new IT2_Consequent("Right", rightMF, direction);
         
 		createLeftWallRulebase();
+		createRightWallRulebase();
+		createObstacleRulebase();
 	}
 	
 	/**
-	 * wallFollow()
+	 * createLeftWallRulebase()
 	 * 
-	 * Creates the wall following fuzzy inference system. Whether right or left
-	 * wall will be determined by the input to the rule base
+	 * Creates left wall following behaviour's rulebase
 	 */
 	private void createLeftWallRulebase() {
 		
@@ -139,8 +188,40 @@ public class SHFLC {
         leftWallRulebase.addRule(new IT2_Rule(new IT2_Antecedent[]{farFront, farBack}, straight));
         
 		//helper. remove when done as will slow down runtime.
-        plotMFs("whatever",new IntervalT2MF_Interface[]{closeMF, farMF},100);
-        plotMFs("whatever",new IntervalT2MF_Interface[]{straightMF, rightMF, leftMF},100);
+        plotMFs("whatever",new IntervalT2MF_Interface[]{obstacleCloseMF, obstacleFarMF},100);
+        //plotMFs("whatever",new IntervalT2MF_Interface[]{straightMF, rightMF, leftMF},100);
+	}
+	
+	/**
+	 * createRightWallRulebase()
+	 * 
+	 * Creates right wall following behaviour's rulebase
+	 */
+	private void createRightWallRulebase() {
+		
+		rightWallRulebase = new IT2_Rulebase(4);
+        rightWallRulebase.addRule(new IT2_Rule(new IT2_Antecedent[]{closeFront, closeBack}, straight));
+        rightWallRulebase.addRule(new IT2_Rule(new IT2_Antecedent[]{closeFront, farBack}, left));
+        rightWallRulebase.addRule(new IT2_Rule(new IT2_Antecedent[]{farFront, closeBack}, right));
+        rightWallRulebase.addRule(new IT2_Rule(new IT2_Antecedent[]{farFront, farBack}, straight));
+	}
+	
+	/**
+	 * createObstacleRulebase()
+	 * 
+	 * Creates obstacle avoidance behaviour's rulebase
+	 */
+	private void createObstacleRulebase() {
+		//change outputs
+		obstacleRulebase = new IT2_Rulebase(8);
+		obstacleRulebase.addRule(new IT2_Rule(new IT2_Antecedent[]{closeLeft, closeMiddle, closeRight}, straight));
+		obstacleRulebase.addRule(new IT2_Rule(new IT2_Antecedent[]{closeLeft, closeMiddle, farRight}, straight));
+		obstacleRulebase.addRule(new IT2_Rule(new IT2_Antecedent[]{closeLeft, farMiddle, closeRight}, straight));
+		obstacleRulebase.addRule(new IT2_Rule(new IT2_Antecedent[]{closeLeft, farMiddle, farRight}, straight));
+		obstacleRulebase.addRule(new IT2_Rule(new IT2_Antecedent[]{farLeft, closeMiddle, closeRight}, straight));
+		obstacleRulebase.addRule(new IT2_Rule(new IT2_Antecedent[]{farLeft, closeMiddle, farRight}, straight));
+		obstacleRulebase.addRule(new IT2_Rule(new IT2_Antecedent[]{farLeft, farMiddle, closeRight}, straight));
+		obstacleRulebase.addRule(new IT2_Rule(new IT2_Antecedent[]{farLeft, farMiddle, farRight}, straight));
 	}
 	
 	
